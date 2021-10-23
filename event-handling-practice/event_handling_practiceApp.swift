e40da4b20cd4b5b7910e7b8fdf44769a60632e6b
Shima <shima.jin@icloud.com>
2021-10-23 23:22:52 +0900

    Initial Commit

---
+++ event-handling-practice/event_handling_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  event_handling_practiceApp.swift
+//  event-handling-practice
+//
+//  Created by Jinsei Shima on 2021/10/23.
+//
+
+import SwiftUI
+
+@main
+struct event_handling_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
